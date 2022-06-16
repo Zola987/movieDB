3da39ea70859bbd7ff0c1cb80259a64509a9e9e1
Zola987 <laza.ivosevic87@gmail.com>
2022-06-16 10:04:42 +0200

    add single movie function

--- src/pages/SingleMovie.jsx
+++ src/pages/SingleMovie.jsx
@@ -1,7 +1,38 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
+import { Link, useParams } from 'react-router-dom';
+import { useGlobalContext } from '../context';
+export const API_ENDPOINT = `https://www.omdbapi.com/?apikey=${process.env.REACT_APP_MOVIE_API_KEY}`;
 
 const SingleMovie = () => {
-  return <div>SingleMovie</div>;
+  const [movie, setMovie] = useState({});
+
+  const { id } = useParams();
+
+  const fetchMovie = async (url) => {
+    const response = await fetch(url);
+    const data = await response.json();
+    setMovie(data);
+  };
+
+  useEffect(() => {
+    fetchMovie(`${API_ENDPOINT}&i=${id}`);
+  }, [id]);
+
+  const { Poster, Title, Plot, Year } = movie;
+  return (
+    <section className="single-container">
+      <img src={Poster} alt={Title} />
+      <div className="single-movie-info">
+        <h2>
+          {Title} <h4>({Year})</h4>
+        </h2>
+        <p>{Plot}</p>
+        <Link className="back-link" to="/">
+          Back to Movies
+        </Link>
+      </div>
+    </section>
+  );
 };
 
 export default SingleMovie;
